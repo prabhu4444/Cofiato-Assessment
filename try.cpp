be5df106dbf622b03589c22ff7ea00968de72aa4
Prabhu Satyam <61291869+prabhu4444@users.noreply.github.com>
2022-06-18 02:58:06 +0530

    Update try.cpp

--- try.cpp
+++ try.cpp
@@ -1,31 +1,21 @@
-
-#include<bits/stdc++.h>
-using namespace std;
-vector<string>v(30, "");
-
-
-int main()
-{
-	v[0] = "1";
-	string s = "1";
-	for (int k = 1; k < 30; k++) {
-		string temp = v[k - 1];
+class Solution {
+public:
+    string countAndSay(int n) {
+     string s = "1";
+	for (int k = 1; k < n; k++) {
+		string temp = "";
 		int c = 0;
 		for (int i = 0; i < s.length(); i++) {
 			if (s[i] != s[i + 1]) {
 				c++;
-				//cout << "s[i]: " << s[i] << endl;
 				temp = temp + to_string(c);
 				temp =  temp + s[i];
-				//cout << temp << endl;
 				c = 0;
 			}
 			else c++;
 		}
-		//c=0;
-		v[k] = temp;
+		s = temp;
 	}
-	int n;
-	cin >> n;
-	cout << v[n] << endl;
-}
+	return s;
+    }        
+};
